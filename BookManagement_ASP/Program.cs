35ec13d40533b733bc0520d584dae1d6d64b3740
Tejkiran Kushwaha <tejkirankushwaha@outlook.com>
2023-08-19 12:06:21 +0530

    Add project files.

---
+++ BookManagement_ASP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BookManagement_ASP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+
+        }
+    }
+}
